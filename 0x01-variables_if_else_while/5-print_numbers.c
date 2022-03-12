bbf7010ed9d70cca326e5eb4c779201b40270ed0
EvansMutwiri <mutwirievansm@gmail.com>
2022-03-12 23:17:48 +0000

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * printf - print numbers
+ * putchar - print character
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = '0';
+	
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+	return (0);
+}
