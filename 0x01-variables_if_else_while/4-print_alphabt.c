de1a1d3645442a3a8483769f65574fbc22b39a21
EvansMutwiri <mutwirievansm@gmail.com>
2022-03-12 22:21:57 +0000

    dont print q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * putchar - print characters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != e)
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar(10);
+	return (0);
+}
+
