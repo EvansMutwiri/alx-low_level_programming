9d03b53df3394826db7eb3b66912288395a3f488
EvansMutwiri <mutwirievansm@gmail.com>
2022-03-12 22:04:22 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * putchar - print characters
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	char ch = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar(10);
+	return (0);
+}
